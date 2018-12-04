e31b037c47df283c49802029fc56ccb865219f56
Rachel Hawa <rachel.hawa@gmail.com>
2018-12-04 23:50:59 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
+require 'pry'
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by! { |string| string.length }
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |str|
+  str[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select {|str| str.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n }
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index != 1
+      element << "s"
+    else
+      element
+    end
+  end
+end
